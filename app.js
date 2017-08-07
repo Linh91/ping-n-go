81029cf8ba6bc6eefa87d63a9b94e05b43a305cc
Ngoc L Ha <sammy_nlh@hotmail.com>
2017-08-07 23:40:40 +0100

    Changed rand for ticket id

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.post('/results', urlencodedParser, function (req, res) {
 	e1_price = require('random-seed').create(seed);
   e1_distance = require('random-seed').create(seed);
 
-  var e1_id = e1_id.intBetween(66, 99);
+  var e1_id = e1_id.intBetween(80, 100);
   var e1_price = e1_price.floatBetween(50, 100).toFixed(2);
   var e1_distance = e1_distance.intBetween(0, 0);
 
@@ -31,7 +31,7 @@ app.post('/results', urlencodedParser, function (req, res) {
 	e2_price = require('random-seed').create(seed);
   e2_distance = require('random-seed').create(seed);
 
-  var e2_id = e2_id.intBetween(33, 66);
+  var e2_id = e2_id.intBetween(40, 60);
   var e2_price = e2_price.floatBetween(20, 100).toFixed(2);
   var e2_distance = e2_distance.intBetween(0, 6);
 
@@ -39,7 +39,7 @@ app.post('/results', urlencodedParser, function (req, res) {
 	e3_price = require('random-seed').create(seed);
   e3_distance = require('random-seed').create(seed);
 
-  var e3_id = e3_id.intBetween(100, 130);
+  var e3_id = e3_id.intBetween(120, 140);
   var e3_price = e3_price.floatBetween(10, 50).toFixed(2);
   var e3_distance = e3_distance.intBetween(6, 12);
 
@@ -47,7 +47,7 @@ app.post('/results', urlencodedParser, function (req, res) {
   e4_price = require('random-seed').create(seed);
   e4_distance = require('random-seed').create(seed);
 
-  var e4_id = e4_id.intBetween(10, 33);
+  var e4_id = e4_id.intBetween(20, 40);
   var e4_price = e4_price.floatBetween(0, 100).toFixed(2);
   var e4_distance = e4_distance.intBetween(12, 17);
 
@@ -55,7 +55,7 @@ app.post('/results', urlencodedParser, function (req, res) {
   e5_price = require('random-seed').create(seed);
   e5_distance = require('random-seed').create(seed);
 
-  var e5_id = e5_id.intBetween(0, 9);
+  var e5_id = e5_id.intBetween(0, 20);
   var e5_price = e5_price.floatBetween(0, 200).toFixed(2);
   var e5_distance = e5_distance.intBetween(17, 25);
 
