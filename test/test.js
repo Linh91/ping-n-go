840a4abdd8f5208df89448708967ab12ee195082
Ngoc L Ha <sammy_nlh@hotmail.com>
2017-08-08 18:46:56 +0100

    First test passed

--- test/test.js
+++ test/test.js
@@ -5,12 +5,22 @@ var baseUrl = "http://localhost:3000/";
 var util = require("util");
 
 describe("Home page", function() {
-  it("should return html", function(done){
+  it("should return 200", function(done){
     request.get({ url: baseUrl },
     function(error, response, body) {
       expect(response.statusCode).to.equal(200);
       console.log(body);
       done();
     });
+
+    it("should return 200", function(done){
+      request.get({ url: baseUrl },
+      function(error, response, body) {
+        var bodyObj = JSON.parse(body);
+        expect(bodyObj.name).to.equal("Luke Skywalker");
+        expect(response.statusCode).to.equal(200);
+        console.log(h1);
+        done();
+      });
   });
 });
