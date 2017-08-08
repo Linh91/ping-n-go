14c6c91b2f3a263c1ec3a607faa7a7ea2debf499
Ngoc L Ha <sammy_nlh@hotmail.com>
2017-08-08 21:17:51 +0100

    Mocha testing html elements

--- test/test.js
+++ test/test.js
@@ -3,24 +3,23 @@ var request = require("request");
 var expect = require("chai").expect;
 var baseUrl = "http://localhost:3000/";
 var util = require("util");
+var module = require("../module");
 
 describe("Home page", function() {
   it("should return 200", function(done){
     request.get({ url: baseUrl },
     function(error, response, body) {
       expect(response.statusCode).to.equal(200);
-      console.log(body);
       done();
     });
-
-    it("should return 200", function(done){
-      request.get({ url: baseUrl },
-      function(error, response, body) {
-        var bodyObj = JSON.parse(body);
-        expect(bodyObj.name).to.equal("Luke Skywalker");
-        expect(response.statusCode).to.equal(200);
-        console.log(h1);
-        done();
-      });
+  });
+  
+  it("has app name Ping! n Go", function(done){
+    request.get({ url: baseUrl },
+    function(error, response, body) {
+      var bodyObj = JSON.stringify(body);
+      expect(bodyObj).to.deep.include("Ping! n Go");
+      done();
+    });
   });
 });
